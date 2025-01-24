0fd317e86db31ea619aef1dbfdc6bc8eb57f8973
RickytheGuy <rickythepsguy@gmail.com>
2025-01-24 13:53:23 -0700

    logging and improved performace

--- curve2flood/cli.py
+++ curve2flood/cli.py
@@ -3,13 +3,40 @@ Command-line interface for Curve2Flood.
 """
 
 import argparse
-from .core import Curve2Flood_MainFunction
+import logging
+
+from curve2flood import Curve2Flood_MainFunction, LOG
+
+def set_log_level(log_level: str):
+    handler = LOG.handlers[0]
+    if log_level == 'debug':
+        LOG.setLevel(logging.DEBUG)
+        handler.setLevel(logging.DEBUG)
+    elif log_level == 'info':
+        LOG.setLevel(logging.INFO)
+        handler.setLevel(logging.INFO)
+    elif log_level == 'warn':
+        LOG.setLevel(logging.WARNING)
+        handler.setLevel(logging.WARNING)
+    elif log_level == 'error':
+        LOG.setLevel(logging.ERROR)
+        handler.setLevel(logging.ERROR)
+    else:
+        LOG.setLevel(logging.WARNING)
+        handler.setLevel(logging.WARNING)
+        LOG.warning('Invalid log level. Defaulting to warning.')
+        return
+        
+    LOG.info('Log Level set to ' + log_level)
 
 def main():
     parser = argparse.ArgumentParser(description="Run Curve2Flood flood mapping tool.")
     parser.add_argument("input_file", help="Path to the input configuration file.")
+    parser.add_argument('-l', '--log', type=str, help='Log Level', 
+                        default='warn', choices=['debug', 'info', 'warn', 'error'])
     args = parser.parse_args()
 
+    set_log_level(args.log)
     Curve2Flood_MainFunction(args.input_file)
 
 if __name__ == "__main__":
